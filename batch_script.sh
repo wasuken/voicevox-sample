fc7221c74162968a0c95c9d1c8a46894f83c0c2f
wasuken <wasuken1@gmail.com>
2024-08-27 22:46:41 +0900

    cron関係すべて修正
    alpine -> debian

--- batch_script.sh
+++ batch_script.sh
@@ -6,17 +6,17 @@
 LOCKFILE="/tmp/batch_script.lock"
 
 # 既にロックファイルが存在するか確認
-if [ -e "${LOCKFILE}" ] && kill -0 `cat ${LOCKFILE}` 2>/dev/null; then
-    echo "Batch job is already running"
-    exit 1
+if [ -e "${LOCKFILE}" ] && kill -0 $(cat ${LOCKFILE}) 2>/dev/null; then
+	echo "Batch job is already running"
+	exit 1
 fi
 
 # ロックファイル作成（プロセスIDを記録）
 trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
-echo $$ > "${LOCKFILE}"
+echo $$ >"${LOCKFILE}"
 
 # バッチ処理の内容をここに記述
-curl -XPOST "http://node:5000/api/voice/path"
+curl -XPOST "http://node:3000/api/voice/path"
 
 # 処理終了後にロックファイルを削除
 rm -f "${LOCKFILE}"
